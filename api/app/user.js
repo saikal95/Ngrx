77dd5406d8359410556d08499041b1b34218f826
saikal <15051995sat>
2022-03-06 18:20:50 +0600

    Validator for repeating emails and displayname and avatar has been added

--- api/app/user.js
+++ api/app/user.js
@@ -1,15 +1,51 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const User = require("../models/User");
+const path = require("path");
+const config = require('../config');
+const multer = require('multer');
+const { nanoid } = require('nanoid');
 
 
 const router = express.Router();
 
 
-router.post('/', async (req, res, next) => {
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, config.uploadPath);
+  },
+  filename: (req, file, cb) => {
+    cb(null, nanoid() + path.extname(file.originalname))
+  }
+});
+
+const upload = multer({storage});
+
+
+
+router.post('/', upload.single('image'),async (req, res, next) => {
+
   try {
+
+      if (!req.body.email || !req.body.password ||!req.body.displayName) {
+        return res.status(400).send({message: 'Email, password or displayName are required'});
+      }
+
+    const userData = {
+      email: req.body.email,
+      password: req.body.password,
+      avatar: null,
+      displayName: req.body.displayName,
+
+    };
+
+    if (req.file) {
+      userData.avatar = req.file.filename;
+    }
     const user = new User(req.body);
+
     user.generateToken();
+
     await user.save();
     return res.send({message: 'New user is created with following id : !', user: {id: user.id}});
   } catch (error) {
--- api/models/User.js
+++ api/models/User.js
@@ -13,9 +13,24 @@ const UserSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  avatar:{
+    type: String,
+  },
+  displayName: {
+    type: String,
+    required: true,
+  },
   token: {
     type: String,
     required: true,
+  },
+  validate: {
+    validator: async value => {
+      const user = await User.findOne({email: value});
+      if(user) return false;
+    },
+    message: 'This user is already registered'
+
   }
 });
 
